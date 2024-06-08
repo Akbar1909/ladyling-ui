e8ecaba1feb9b6c3983a82348ddfd8cf04de0a84
Akbar Bobomurodov <dev.bobomurodov@gmail.com>
2024-06-08 16:11:44 +0500

    fix

--- src/app/challenges/[id]/page.jsx
+++ src/app/challenges/[id]/page.jsx
@@ -12,30 +12,32 @@ const Challenge = async (props) => {
 
   return (
     <PageWrapper>
-      <div className="flex flex-col space-y-4 space-y-reverse items-center">
-        <h1 className="order-last text-lg text-gray-700">
-          Participate, Learn, and Win Prizes!
-        </h1>
-        <h2 className="text-5xl font-bold text-gray-900 sm:text-6xl">
-          {data?.name}
-        </h2>
-      </div>
-      <p className="mx-auto mt-6 max-w-xl text-pretty text-base/relaxed text-gray-700">
-        {data.description}
-      </p>
+      <div className="min-w-[300px] flex justify-center flex-col items-center">
+        <div className="flex flex-col space-y-4 space-y-reverse items-center">
+          <h1 className="order-last text-lg text-gray-700">
+            Participate, Learn, and Win Prizes!
+          </h1>
+          <h2 className="text-5xl font-bold text-gray-900 sm:text-6xl">
+            {data?.name}
+          </h2>
+        </div>
+        <p className="mx-auto mt-6 max-w-xl text-pretty text-base/relaxed text-gray-700">
+          {data.description}
+        </p>
 
-      <StartButton />
+        <StartButton />
 
-      <div className="mt-8 w-full">
-        <Suspense
-          fallback={
-            <h1 className="text-3xl text-center">
-              Getting the leader board ⏰
-            </h1>
-          }
-        >
-          <LeaderBoard testId={props.params.id} />
-        </Suspense>
+        <div className="mt-8 w-full">
+          <Suspense
+            fallback={
+              <h1 className="text-3xl text-center">
+                Getting the leader board ⏰
+              </h1>
+            }
+          >
+            <LeaderBoard testId={props.params.id} />
+          </Suspense>
+        </div>
       </div>
     </PageWrapper>
   );
--- src/components/EnterButton/EnterButton.jsx
+++ src/components/EnterButton/EnterButton.jsx
@@ -1,5 +1,4 @@
 "use client";
-import useGetMe from "@/hooks/api/useGetMe";
 import useAuthStore from "@/providers/zustand/auth";
 import useEnterModal from "@/providers/zustand/ui";
 import { Button } from "flowbite-react";
@@ -8,10 +7,9 @@ import React from "react";
 const EnterButton = () => {
   const toggle = useEnterModal((state) => state.toggle);
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  const { data } = useGetMe();
 
   if (isAuthenticated) {
-    return <span className="text-xl">{data?.username || ""}</span>;
+    return null;
   }
 
   return (
